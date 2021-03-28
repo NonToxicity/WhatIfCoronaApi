c3e21a06905a2a4ab11b778cc1bd7c5069133f77
Mert Kaya <47145541+mertkayacs@users.noreply.github.com>
2021-03-28 22:55:09 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -22,37 +22,45 @@ def home():
     arg = request.args['predict']
     load()
     if(loaded_model != None):
-        result = arg
-        predicted = ['Afghanistan',0,37.746,2.581,8.33,38928341,0,0,0]
+        prediction = arg
+        predicted = prediction.split(",")
+        #predicted = ['Afghanistan',300,37.746,2.581,8.33,38928341,0,0,0]
         a = numpy.asarray(predicted)
 
-        df = pd.DataFrame(my_array, columns = ['location','date','handwashing_facilities','aged_65_older','stringency_index','population','total_vaccinations','female_smokers','male_smokers'])
+        df = pd.DataFrame([a], columns = ['location','date','handwashing_facilities','aged_65_older','stringency_index','population','total_vaccinations','female_smokers','male_smokers'])
 
         categorical_cols = ['location'] 
         # apply le on categorical feature columns
-        df[categorical_cols] = df[categorical_cols].apply(lambda col: le.fit_transform(col))    
+        df[categorical_cols] = df[categorical_cols].apply(lambda col: label_encoder.transform(col))    
 
-        array_hot_encoded = ohe.fit_transform(df[categorical_cols])
+        #array_hot_encoded = oneHotEncoder.fit_transform(df[categorical_cols])
         
         #Convert it to df
-        data_hot_encoded = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
+        #data_hot_encoded = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
         
         #Extract only the columns that didnt need to be encoded
-        data_other_cols = df.drop(columns=categorical_cols)
+        #data_other_cols = df.drop(columns=categorical_cols)
         
         #Concatenate the two dataframes : 
-        data_out = pd.concat([data_hot_encoded, data_other_cols], axis=1)
+        #data_out = pd.concat([data_hot_encoded, data_other_cols], axis=1)
 
-        data_out = data_out.applymap(float)
+        #enc_df = pd.DataFrame(data=oneHotEncoder.transform(df).toarray(), columns=oneHotEncoder.get_feature_names(col_names), dtype=bool)
+        #df = pd.concat([data_df, enc_df], axis=1)
+        df = df.applymap(float)
+        temp = df.iloc[0]
+        temp = temp.values.reshape(1,-1) 
         
-        result = loaded_model.predict(data_out)
+        result = loaded_model.predict(temp)
+
     return str(result)
 
 
 def load():
     global loaded_model
+    global label_encoder
+    #global oneHotEncoder
     loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
     label_encoder = LabelEncoder()
     label_encoder.classes_ = numpy.load('classesLabelEncoder.npy',allow_pickle=True)
-    oneHotEncoder = joblib.load('encoder.joblib')
+    #oneHotEncoder = joblib.load('encoder.joblib')
         
